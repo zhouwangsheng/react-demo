2a57fe8545646abcc8c8be172b1a8b03d99b1c5b
zhouwangsheng <1359170974@qq.com>
2016-07-02 22:09:49 +0800

    all

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,15 +3,49 @@ require('styles/App.css');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+let imageDatas  = require('../data/imageData.json')
+
+imageDatas = (function(imageDataArr){
+    for(var i = 0,j=imageDataArr.length;i<j;i++){
+        var sImgData = imageDataArr[i];
+        sImgData.imageUrl = require('../img/' + sImgData.fileName )
+
+        imageDataArr[i] = sImgData
+    }
+    return imageDataArr;
+})(imageDatas)
+
+class ImageFigure extends React.Component {
+    render(){
+        return (
+            <figure>
+                <img/>
+                <figcaption>
+                      <h2></h2>
+                </figcaption>
+            </figure>
+        )
+
+    }
+}
 
 class AppComponent extends React.Component {
   render() {
+    var controllerUnits = [],
+        imgFigures = [];
+
+    imageDatas.forEach(function(item){
+        imgFigures.push(<ImageFigure data={item}>)
+    })
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <section className="stage">
+            <section className="img-sec">
+                {imgFigures}
+            </section>
+            <nav className="controller-nav">
+                {controllerUnits}
+            </nav>
+      </section>
     );
   }
 }
